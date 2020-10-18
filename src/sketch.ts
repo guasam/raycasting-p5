6aabe02b91c52d3812203c0735ef070ae2836025
guasam <guasam@codesbiome.com>
2020-10-18 13:23:59 +0530

    set default debug false

--- src/sketch.ts
+++ src/sketch.ts
@@ -9,7 +9,7 @@ const sketch = (p5: P5) => {
   let walls: Wall[] = [];
   const walls_count = 6;
   const total_rays = 360;
-  const debug = false;
+  const debug = true;
 
   /**
    * This function is called once when the program starts.
